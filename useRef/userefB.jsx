ed6fd041ed24907e996d0ed9f81546710ca0e685
Ram Harsh Pandey <120307005+ram-ethan@users.noreply.github.com>
2024-10-10 18:04:54 +0530

    Add files via upload

---
+++ useRef/userefB.jsx
@@ -0,0 +1,47 @@
+import { useState, useRef } from 'react'
+
+import './App.css'
+
+function App() {
+  const [time, setTime] = useState(0);
+  const timeRef = useRef(null);
+
+  function handleStart() {
+    timeRef.current = setInterval(() => {
+      setTime(time => time + 1);
+    }, 1000);
+  }
+  function handleStop() {
+    clearInterval(timeRef.current);
+  }
+  function handleReset() {
+    clearInterval(timeRef.current);
+    setTime(0);
+  }
+
+  return (
+    <>
+      <h1>StopWatch : {time} seconds</h1>
+      <br />
+      <br />
+      <div >
+        <button onClick={handleStart}>Start</button>
+
+      </div>
+      <br />
+      <br />
+      <div >
+        <button onClick={handleStop}>Stop</button>
+
+      </div>
+      <br />
+      <br />
+      <div>
+        <button onClick={handleReset}>Reset</button>
+
+      </div>
+    </>
+  )
+}
+
+export default App
